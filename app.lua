c2a2bf873763dcc0593b032ec98da4746dd290a8
bscal <jonfus123@hotmail.com>
2020-03-06 08:16:15 +0000

    Updates to app.lua - Added return of json string of data - Added LogErr function - Added framework for character's and character api

--- app.lua
+++ app.lua
@@ -1,5 +1,6 @@
 local lapis = require("lapis")
 local config = require("lapis.config").get()
+local util = require("lapis.util")
 local Model = require("lapis.db.model").Model
 
 local app = lapis.Application()
@@ -8,14 +9,20 @@ local Users = Model:extend("users", {
     primary_key = "steamid"
 })
 
+local Players = Model:extend("players", {
+    primary_key = "steamid"
+})
+
 
 app:get("/", function()
-    --return { json = { hello = "test"}}
     return { layout = false, "This is a test"}
 end)
 
 app:match("/login/:steamid[%d]", function(self)
-  	--return { json = { hello = "test"}}
+    if not self.params.steamid then
+        return LogErr("no steamid")
+    end
+
     local user = Users:find(self.params.steamid)
 
     if not user then
@@ -25,7 +32,26 @@ app:match("/login/:steamid[%d]", function(self)
 		print("User exists...")
     end
 
-    return { layout = false, self.params.steamid}
+    return { json = {user} }
 end)
+
+app:match("/player/:steamid[%d]/:cid[%d]", function(self)
+    if not self.params.steamid then
+        return LogErr("no steamid")
+    end
+
+    if not self.params.cid then
+        return LogErr("no character id")
+    end
+
+    print(self.params.steamid)
+    print(self.params.cid)
+
+end)
+
+function LogErr(msg)
+    print("[Error]: " .. msg)
+    return { status = 400, layout = false, "[Error]: " .. msg }
+end
 
 return app
