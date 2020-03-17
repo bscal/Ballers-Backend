78f13892ab7692a910623857f1dddf9be8a4c8d9
bscal <jonfus123@hotmail.com>
2020-03-17 03:32:18 +0000

    Added a check in config.lua to make sure you edited credentials.lua values

--- config.lua
+++ config.lua
@@ -2,6 +2,10 @@
 local config = require("lapis.config")
 local credentials = require("credentials")
 
+if credentials.mysql.password == "password" then
+  print("[Warning] If you a receiving mysql errors you might have forgotten to change credentials.lua values!")
+end
+
 config("development", {
   port = 9090,
   mysql = credentials.mysql
